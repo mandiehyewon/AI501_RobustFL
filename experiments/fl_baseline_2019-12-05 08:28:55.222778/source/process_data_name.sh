ff2dafab02e401b01be19cf1c32948262e890387
jin8 <kwak.jinmyung@gmail.com>
2019-12-05 08:35:35 +0000

    fl_working & adv.attack

--- experiments/fl_baseline_2019-12-05 08:28:55.222778/source/process_data_name.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-IFILE=/st2/myung/data/diabetic-retinopathy-detection/kaggle/trainLabels.csv
-mkdir train_processed
-mkdir val_processed
-awk -F, '(NR<30000){system("cp train/"$1".jpeg train_processed/"$1"_"$2".jpeg ")} (NR>=30000){system("cp train/"$1".jpeg val_processed/"$1"_"$2".jpeg ")}' $IFILE
