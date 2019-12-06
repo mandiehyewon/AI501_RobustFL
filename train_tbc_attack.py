f9565b3110ccc521593f0e63c8537b4ac8399ff5
jin8 <kwak.jinmyung@gmail.com>
2019-12-06 22:03:45 +0900

    add image generation

--- train_tbc_attack.py
+++ train_tbc_attack.py
@@ -14,7 +14,9 @@ from art.attacks import FastGradientMethod, CarliniLInfMethod, ProjectedGradient
 # others works in image size 32x32
 dataset_type = "tbc"
 experiment = "all"
-cnn_type ="vgg19"
+cnn_type = "vgg19"
+best_acc = 0.864
+example = 0
 # configure
 HEIGHT = 224
 WIDTH = 224
@@ -81,29 +83,55 @@ if __name__ == "__main__":
     for layer in base_model.layers:
         layer.trainable = False
 
-    """
-    cnn.compile(
-        loss="sparse_categorical_crossentropy",
-        optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
-        metrics=[tf.keras.metrics.SparseCategoricalAccuracy("acc")],
-    )
-    """
 
     cnn.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
               loss=tf.keras.losses.SparseCategoricalCrossentropy(),
               metrics=[tf.keras.metrics.SparseCategoricalAccuracy("acc")])
 
     print(cnn.summary())
+    if not os.path.isfile('basemodel-{:0.3f}.h5'.format(best_acc)):
+        result = cnn.fit_generator(
+            train_ds,
+            verbose=1,
+            epochs=EPOCHS,
+            steps_per_epoch=STEPS_PER_EPOCH,
+            validation_data=test_ds,
+            validation_steps=1)
 
-    result = cnn.fit_generator(
-        train_ds,
-        verbose=1,
-        epochs=EPOCHS,
-        steps_per_epoch=STEPS_PER_EPOCH,
-        validation_data=test_ds,
-        validation_steps=1)
+        # evaluate
+        print()
+        predict_classes = []
+        x_test = []
+        y_test = []
 
-    # evaluate
+        # one batch for validation
+        for x,y in test_ds.take(1):
+            predict = cnn.predict(x)
+            predict_classes = np.argmax(predict, axis=-1)
+            x_test = np.array(x, dtype=np.float32)
+            y_test = np.array(y, dtype=np.int32)
+
+
+        print(y_test.shape, predict_classes.shape)
+        from sklearn.metrics import classification_report
+        target_names = ["Class {}".format(i) for i in range(CLASSES)]
+        print(classification_report(y_test, predict_classes, target_names=target_names))
+
+
+        score = cnn.evaluate(x_test, y_test,verbose=1)
+        print()
+        print('test loss:', score[0])
+        print('test accuracy:', score[1])
+
+        cnn.save('basemodel-{:0.3f}.h5'.format(score[1]))
+    else:
+        cnn.load_weights('basemodel-{:0.3f}.h5'.format(best_acc))
+        print("Loaded model")
+
+
+    # plot result
+    #import pdb
+    #pdb.set_trace()
     print()
     predict_classes = []
     x_test = []
@@ -127,14 +155,6 @@ if __name__ == "__main__":
     print()
     print('test loss:', score[0])
     print('test accuracy:', score[1])
-
-    cnn.save('no_attack_{}_{}_{}_img{}x{}-{:0.3f}.h5'.format(
-        experiment, cnn_type, dataset_type, HEIGHT, WIDTH, score[1]
-    ))
-    # plot result
-    #import pdb
-    #pdb.set_trace()
-
     #tf.compat.v1.disable_eager_execution()
     classifier = KerasClassifier(model=cnn, clip_values=(0, 1))
 
@@ -143,6 +163,7 @@ if __name__ == "__main__":
     predict = classifier.predict(x_test)
     accuracy = np.sum(np.argmax(predict, axis=1) == y_test) / len(y_test)
     print('Accuracy on benign test examples: {:.3f}%'.format(accuracy * 100))
+    x_example = x_test[example]
 
     # Generate adversarial test examples
     # Evaluate the ART classifier on adversarial test examples
@@ -157,6 +178,7 @@ if __name__ == "__main__":
     print(classification_report(y_test, predict_classes, target_names=target_names))
     accuracy = np.sum(np.argmax(predict, axis=1) == y_test) / len(y_test)
     print('Accuracy on FastGradientMethod test examples: {:.3f}%'.format(accuracy * 100))
+    fg_example = x_test_adv[example]
 
     print("*"*100)
     attack = CarliniLInfMethod(classifier=classifier, eps=0.3, max_iter=100, learning_rate=0.01)
@@ -169,6 +191,7 @@ if __name__ == "__main__":
     print(classification_report(y_test, predict_classes, target_names=target_names))
     accuracy = np.sum(np.argmax(predict, axis=1) == y_test) / len(y_test)
     print('Accuracy on CarliniLInfMethod test examples: {:.3f}%'.format(accuracy * 100))
+    carlini_example = x_test_adv[example]
 
     print("*"*100)
     attack = ProjectedGradientDescent(classifier, norm=np.inf, eps=0.3, eps_step=0.1,max_iter=100)
@@ -181,19 +204,20 @@ if __name__ == "__main__":
     print(classification_report(y_test, predict_classes, target_names=target_names))
     accuracy = np.sum(np.argmax(predict, axis=1) == y_test) / len(y_test)
     print('Accuracy on ProjectedGradientDescent[norm=inf] test examples: {:.3f}%'.format(accuracy * 100))
+    pgdInf_example = x_test_adv[example]
 
     print("*"*100)
     attack = ProjectedGradientDescent(classifier, norm=1, eps=0.3, eps_step=0.1,max_iter=100)
     x_test_adv = attack.generate(x_test)
     perturbation = np.mean(np.abs((x_test_adv - x_test)))
     print('Average perturbation: {:.10f}'.format(perturbation))
-    predictions = classifier.predict(x_test_adv)
-    predict_classes = np.argmax(predictions, axis=-1)
+    predict = classifier.predict(x_test_adv)
+    predict_classes = np.argmax(predict, axis=-1)
     target_names = ["Class {}".format(i) for i in range(CLASSES)]
     print(classification_report(y_test, predict_classes, target_names=target_names))
-
     accuracy = np.sum(np.argmax(predict, axis=1) == y_test) / len(y_test)
     print('Accuracy on ProjectedGradientDescent[norm=1] test examples: {:.3f}%'.format(accuracy * 100))
+    pgd1_example = x_test_adv[example]
 
     print("*"*100)
     attack = ProjectedGradientDescent(classifier, norm=2, eps=0.3, eps_step=0.1,max_iter=100)
@@ -206,3 +230,18 @@ if __name__ == "__main__":
     print(classification_report(y_test, predict_classes, target_names=target_names))
     accuracy = np.sum(np.argmax(predict, axis=1) == y_test) / len(y_test)
     print('Accuracy on ProjectedGradientDescent[norm=2] test examples: {}%'.format(accuracy * 100))
+    pgd2_example = x_test_adv[example]
+
+    imgs = [x_example, fg_example, carlini_example,\
+            pgdInf_example, pgd1_example, pgd2_example]
+    img_labels = ["Original", "FastGradient", "CarliniInf",\
+                  "PGD[norm=Inf]", "PGD[norm=1]", "PGD[norm=2]"]
+    fig=plt.figure(figsize=(20, 20))
+    columns = 6
+    rows = 1
+    for i in range(0, columns*rows):
+        fig.add_subplot(rows, columns, i+1)
+        plt.imshow(imgs[i])
+        plt.title(img_labels[i])
+
+    plt.savefig("attack_example2.png")
